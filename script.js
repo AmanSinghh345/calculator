98becf4d22434ddd8d8972e421460fc1054dd657
Aman <349aman349@gmail.com>
2025-05-18 18:15:32 +0530

    Added Comment

--- script.js
+++ script.js
@@ -10,6 +10,8 @@ function clearDisplay() {
 
 function calculate() {
   try {
+    //eval() is a JavaScript function that evaluates a string as code and executes it.
+//It's powerful but risky, as it can run malicious or unintended code if not used carefully.
     display.value = eval(display.value);
   } catch (error) {
     display.value = "Error";
